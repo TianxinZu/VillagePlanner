b96c1daaa611fc1f7edda4d5cabd9a993849463e
Tianxin Zu <2252922521@qq.com>
2022-10-29 23:20:59 -0700

    get current location on map

--- app/src/main/java/com/villageplanner/MainActivity.java
+++ app/src/main/java/com/villageplanner/MainActivity.java
@@ -1,21 +1,32 @@
 package com.villageplanner;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap mapAPI;
     SupportMapFragment mapFragment;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
         mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Override
@@ -31,8 +43,59 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapAPI = googleMap;
         LatLng USCVillage = new LatLng(34.0256, -118.2850);
         mapAPI.addMarker(new MarkerOptions().position(USCVillage).title("USCVillage"));
-        mapAPI.animateCamera(CameraUpdateFactory.newLatLngZoom(USCVillage, 16.0f));
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(USCVillage, 17.0f));
+
+        getLocationPermission();
+        getDeviceLocation();
     }
 
 
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        }
+        else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        System.out.println(locationPermissionGranted == true);
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, task -> {
+                    System.out.println(task.isSuccessful() == false);
+                    if (task.isSuccessful()) {
+                        // Set the map's camera position to the current location of the device.
+                        Location currentLocation = task.getResult();
+//                        assert currentLocation == null : "null";
+                        System.out.println(currentLocation == null);
+                        if (currentLocation != null) {
+                            LatLng current = new LatLng(currentLocation.getLatitude(),
+                                    currentLocation.getLongitude());
+                            mapAPI.addMarker(new MarkerOptions().position(current).title("Current Location"));
+                            mapAPI.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    current, 15));
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
 }
